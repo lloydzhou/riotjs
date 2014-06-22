eaa24ab72e8f5db44af3b429e3fddb0fa7f0f732
lloydzhou <lloydzhou@qq.com>
2014-06-23 00:34:58 +0800

    add model method

--- riot.js
+++ riot.js
@@ -1,5 +1,20 @@
 /* Riot 0.9.9, @license MIT, (c) 2014 Moot Inc + contributors */
 (function($) { "use strict";
+var models = {}, guid = function(){return Math.random().toString(36).substring(2, 15)}
+$.model = function(attr, parent) {
+    // create new object: obj = $.model(name, data)
+    // define one class: class = $.model(attr, parent)
+    if (typeof attr == 'string') 
+        return models[attr] ? new models[attr](parent) : {}
+    var pobj = typeof parent == "function" ? new parent() : 
+        typeof parent == 'string' && $.model(parent)
+    return models[attr.name || guid()] = function(data) {
+        $.extend(this, pobj, attr, data)
+        if (!(this.on && this.off && this.trigger && this.one)) $.observable(this)
+        this.name = attr.name || this.name || arguments.callee.name || 'base'
+        this[this.name+'Init'] && this[this.name+'Init'](this)
+    }
+} 
 
 $.observable = function(el) {
   var callbacks = {}, slice = [].slice;
